23e3b0419243b6f0e9bdcfc4be12d12c5a12aa66
Faris Maleab <farisnt@gmail.com>
2021-05-31 14:04:53 +0400

    V1
    Added the support to send EMail notification for the agent about the status.

--- FSLogixMigration/FSLogixMigration.ps1
+++ FSLogixMigration/FSLogixMigration.ps1
@@ -13,10 +13,13 @@ How to use:
 Please read the Post in www.powershellcenter.com for more information
 
 #Parameters:
-PickUpFolder: The location where the script will read the Move request. excpected value any share folder \\Sharefolder\... 
-VHDorVHDX: the format of the Old FSLogix container, possible value "VHD" or "VHDX".
-OldProfilePath: the location of the old FSLogix containers. Expected value: \\FileServer\FSLogix Profile 
-NewDeliveryCTRL: The New XenDesktop environement delivery controller name. Expected value MyVDIEnv.domain.local
+PickUpFolder [ String ]: The location where the script will read the Move request. excpected value any share folder \\Sharefolder\... 
+VHDorVHDX [ String - Validation Set]: the format of the Old FSLogix container, possible value "VHD" or "VHDX".
+OldProfilePath [ String ]: the location of the old FSLogix containers. Expected value: \\FileServer\FSLogix Profile 
+NewDeliveryCTRL [ String ]: The New XenDesktop environement delivery controller name. Expected value MyVDIEnv.domain.local
+SMTPEnabled [ switch ]: If presented the script will send email notification to the helpdesk agent.
+SMTPServer [ String ]: SMTP Server to relay the message to.
+
 
 
 
@@ -24,14 +27,25 @@ NewDeliveryCTRL: The New XenDesktop environement delivery controller name. Expec
 
 #requires -modules Citrix.XenDesktop.Admin
 #requires -RunAsAdministrator
+#required -modules Hyper-V
 
-[Cmdletbinding()]
+[Cmdletbinding(DefaultParameterSetName='vdi')]
 param(
-[parameter(Mandatory=$true)]$PickUpFolder,
-[parameter(Mandatory=$false)]
-        [ValidateSet("VHD", "VHDX")]$VHDorVHDX='VHDX',
-[parameter(Mandatory=$true)]$OldProfilePath,
-[parameter(Mandatory=$true)]$NewDeliveryCTRL
+[parameter(Mandatory=$true,ParameterSetName='vdi')]
+[parameter(ParameterSetName='Email')]$PickUpFolder,
+
+[parameter(Mandatory=$false,ParameterSetName='vdi')]
+[ValidateSet("VHD", "VHDX")]
+[parameter(ParameterSetName='Email')]$VHDorVHDX='VHDX',
+
+[parameter(Mandatory=$true,ParameterSetName='vdi')]
+[parameter(ParameterSetName='Email')]$OldProfilePath,
+
+[parameter(Mandatory=$false,ParameterSetName='Email')][switch]$SMTPEnabled,
+[parameter(Mandatory=$true,ParameterSetName='Email')]$SMTPServer,
+[parameter(Mandatory=$false,ParameterSetName='Email')]$SMTPSender="vdimigration@domain.com",
+[parameter(Mandatory=$true,ParameterSetName='vdi')]
+[parameter(ParameterSetName='Email')]$NewDeliveryCTRL
 )
 
 Function Set-Loginfo {
@@ -57,7 +71,10 @@ While($true){
 $ReadAllFiles=Get-ChildItem -Filter *.dat -Path $PickUpFolder    
     if (!($ReadAllFiles.count -eq 0)){    
     foreach($File in $ReadAllFiles){      
-        $UserToMigrate=Get-Content $File.FullName
+        $UserToMigrate=Get-Content $File.FullName -ErrorAction Stop
+         $FolderToCopy=((($UserToMigrate[0]).Split("="))[1]).Split(",")
+        $FilesToCopy=((($UserToMigrate[1]).Split("="))[1]).Split(",")
+        $SupportAgent=((($UserToMigrate[2]).Split("="))[1]).Split(",")
         $User=($File.Name).Remove(($file.name.Length) -4)
         $GBS=@{
               AdminAddress=$NewDeliveryCTRL
@@ -66,21 +83,23 @@ $ReadAllFiles=Get-ChildItem -Filter *.dat -Path $PickUpFolder
 
             if ((Get-BrokerMachine @GBS)){$NewVDIName=(Get-BrokerMachine @GBS).MachineName.Substring(($env:USERDOMAIN.Length)+1)}
               Else{
-            Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg "No Active Session Found!"
+         Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg "No Active Session Found!"
+         Rename-Item -Path $File.FullName -NewName "$($File.Name).Error.$(Get-Date -Format "ss-mm-hh")"
+         if ($SMTPEnabled){Send-MailMessage -To $SupportAgent -From $SMTPSender -Body "No Active Session for $($User)"  -SmtpServer $SMTPServer -Subject "User Migration for $($file.Name)" }
+        break
             }
-        $FolderToCopy=((($UserToMigrate[0]).Split("="))[1]).Split(",")
-        $FilesToCopy=((($UserToMigrate[1]).Split("="))[1]).Split(",")
+        if ($SMTPEnabled){Send-MailMessage -To $SupportAgent -From $SMTPSender -Body "Process Started for  $($User), Please wait while processing..."  -SmtpServer $SMTPServer -Subject "User Migration for $($file.Name)" }
         Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg $File 
         $UserSid = (Get-ADUser -Identity $User).sid.value
         Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg $UserSid
-        $UserDisk=Get-ChildItem -Filter "*.$($VHDorVHDX)" -Path $OldProfilePath -Recurse | Where-Object {((($_.Directory.name.Split("_"))[0]) -like $UserSid) -or ((($_.Directory.name.Split("_"))[1]) -like $UserSid)}
+        $UserDisk=Get-ChildItem -Filter "*.$($VHDorVHDX)" -Path $OldProfilePath -Recurse -ErrorAction  Stop | Where-Object {((($_.Directory.name.Split("_"))[0]) -like $UserSid) -or ((($_.Directory.name.Split("_"))[1]) -like $UserSid)}
         try{
             Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg "Disk name is $($UserDisk)"
             Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg "Prepare to Mount the VHD/x"
             Mount-VHD -Path $UserDisk.FullName -ReadOnly -ErrorAction Stop
           }
         Catch{
-         Rename-Item -Path $File.FullName -NewName $("$File.Error")
+         Rename-Item -Path $File.FullName -NewName $("$File.Error".$(Get-Date -Format "ss-mm-hh"))
          Remove-item -Path $File.FullName
         }
         $DiskName=$UserDisk.Name.Replace("_","-").substring(0,($UserDisk.Name.Length -($VHDorVHDX.Length)-1))
@@ -125,9 +144,15 @@ $ReadAllFiles=Get-ChildItem -Filter *.dat -Path $PickUpFolder
                 }
                 Remove-PartitionAccessPath @RPAP
                 Dismount-VHD -DiskNumber $DiskInfo.Disknumber
+                $filenameToSend=$file.FullName.Replace(".dat",".log")
+                Write-Host "Sending email"
+                if ($SMTPEnabled){Send-MailMessage -To $SupportAgent -From $SMTPSender -Body "Results for user $($User), Please check the attachemnet"  -SmtpServer $SMTPServer -Subject "User Migration for $($file.Name)" -Attachments $filenameToSend}
+                write-host "Email Sent."
+
                 Start-Sleep -Seconds 2
                 Remove-Item -Path "C:\disks\$($diskname)"
                 remove-item -Path $File.FullName
+                
 
 
       }
@@ -140,6 +165,9 @@ $ReadAllFiles=Get-ChildItem -Filter *.dat -Path $PickUpFolder
         Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg $Error[0]
         Set-Loginfo -UserLog $User -dirToWrite $PickUpFolder -txtmsg  (Get-Volume)
         write-host $Error[0].Exception
+        $filenameToSend=$file.FullName.Replace(".dat",".log")
+        if ($SMTPEnabled){Send-MailMessage -To $SupportAgent -From $SMTPSender -Body "Results for user $($User), Please check the attachemnet"  -SmtpServer $SMTPServer -Subject "User Migration for $($file.Name)" -Attachments $filenameToSend}
         Rename-Item -Path $File.FullName -NewName $("$File.Error")
+
         break
         }
