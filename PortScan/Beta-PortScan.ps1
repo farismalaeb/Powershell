34611bd75571fc03f800c0f3d17411b4efc47730
Faris Malaeb <60314774+farismalaeb@users.noreply.github.com>
2021-11-22 13:19:20 +0400

    Create Beta-PortScan.ps1
    Test First version of Online Port Scanning

---
+++ PortScan/Beta-PortScan.ps1
@@ -0,0 +1,90 @@
+﻿[cmdletbinding()]
+param(
+[parameter(Mandatory=$true)]$HostToScan,
+[parameter(Mandatory=$true,ParameterSetName="Range")]$StartingPort,
+[parameter(Mandatory=$true,ParameterSetName="Range")]$EndingPort,
+[parameter(Mandatory=$true,ParameterSetName="SelectivePort")][array]$SelectivePort
+)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$OpenPorts=@()
+
+if ($PSCmdlet.ParameterSetName -like "Range"){
+    $Body=@{
+    remoteHost =$HostToScan
+    start_port = $PSBoundParameters['StartingPort']
+    end_port = $PSBoundParameters['EndingPort']
+    normalScan = 'Yes'
+    scan_type = 'connect'
+    ping_type = 'none'
+    }
+
+
+
+try{
+
+    $WebResult=(Invoke-RestMethod -Method Post -Body $Body -Uri 'https://www.ipfingerprints.com/scripts/getPortsInfo.php' -ErrorAction Stop).PortScanInfo.Split("`n") |where {$_ -match "(^\d*)\/\D{3}"}
+    foreach ($Singleline in $WebResult){
+
+    $FilteredResult=[pscustomobject]@{
+        Host=$PSBoundParameters['HostToScan']
+        Port=""
+        Result=""
+    }
+      $FilteredResult.Port=([regex]::Matches($Singleline,"^\d*")).value
+      if ($Singleline -like "*open*"){$FilteredResult.Result="Open"}
+      Else{$FilteredResult.Result="Filtered"}
+        $OpenPorts+=$FilteredResult
+    }
+
+
+}
+
+
+Catch{
+Write-Host $_.exception.Message
+}
+}
+
+if ($PSCmdlet.ParameterSetName -like "SelectivePort"){
+
+
+
+foreach ($SinglePort in $SelectivePort){
+
+    $Body=@{
+        remoteHost =$HostToScan
+        start_port = $SinglePort
+        end_port = $SinglePort
+        normalScan = 'Yes'
+        scan_type = 'connect'
+        ping_type = 'none'
+        }
+    
+       $SingleFilteredResult=[pscustomobject]@{
+        Host=$PSBoundParameters['HostToScan']
+        Port=""
+        Result=""
+        }
+
+
+        Try{
+    $SingleWebRequest=(Invoke-RestMethod -Method Post -Body $Body -Uri 'https://www.ipfingerprints.com/scripts/getPortsInfo.php' -ErrorAction Stop).PortScanInfo.Split("`n") |where {$_ -match "(^\d*)\/\D{3}"}
+    
+      $SingleFilteredResult.Port=([regex]::Matches($SingleWebRequest,"^\d*")).value
+      if ($SingleWebRequest -like "*open*"){ $SingleFilteredResult.Result="Open"}
+      Else{ $SingleFilteredResult.Result="Filtered"}
+        $OpenPorts+=$SingleFilteredResult
+    }
+    catch{
+    Write-Host $_.exception.Message
+
+    }
+    
+  }
+
+}    
+    
+    $OpenPorts
+
+
+
