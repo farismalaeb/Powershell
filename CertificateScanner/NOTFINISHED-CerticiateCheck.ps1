d4fd9647d5b08032b9a7a1451b0ea969a9b0587f
Faris Malaeb <60314774+farismalaeb@users.noreply.github.com>
2021-12-19 19:12:25 +0400

    Update NOTFINISHED-CerticiateCheck.ps1

--- CertificateScanner/NOTFINISHED-CerticiateCheck.ps1
+++ CertificateScanner/NOTFINISHED-CerticiateCheck.ps1
@@ -1,17 +1,28 @@
 ﻿param(
 [parameter(mandatory=$true)]$FilePath,
-[parameter(mandatory=$true)]$NoCertValidation,
-[parameter(mandatory=$true)]
-[parameter(mandatory=$true)]
-[validateset("TLS1.0","TLS1.1","TLS1.2","SSLv3","SSLv2")]$ProtocolVersion,
-[parameter(mandatory=$true)]$SaveAsTo
+[parameter(mandatory=$false)]$NoCertValidation=$true,
+[parameter(mandatory=$false)]
+[validateset("Tls","TLS11","Tls12","Ssl3","SystemDefault")]$ProtocolVersion='TLS11',
+[parameter(mandatory=$false)]$SaveAsTo
 )
 
-if (!(Test-Path $FilePath)){Throw "Path Incorrect"}
-
-$CertificateList=Get-Content -Path "C:\Users\rescu\cert.txt"
+if (!(Test-Path $FilePath)){Throw "Incorrect Source Path."}
+$Fullresult=@()
+$CertificateList=Get-Content -Path $FilePath
 
 Foreach($url in $CertificateList){
+
+    switch ($NoCertValidation){
+        $True  {[Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }}
+        $false {[Net.ServicePointManager]::ServerCertificateValidationCallback = { $False}}
+    }
+
+    if ($PSBoundParameters.Keys -like "ProtocolVersion"){
+         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::$ProtocolVersion
+    }
+
+
+
 [Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 Try{
 $results=[PSCustomObject]@{
@@ -24,12 +35,13 @@ $req = [Net.HttpWebRequest]::Create($url)
 $req.GetResponse() |Out-Null
 $results.URL=$url
 $results.StartDate=$req.ServicePoint.Certificate.GetEffectiveDateString()
-    if ($req.ServicePoint.Certificate.GetExpirationDateString() -le (Get-Date).Date){
+    if ([datetime]$req.ServicePoint.Certificate.GetExpirationDateString() -le (Get-Date).Date){
     Write-Host $url -NoNewline -ForegroundColor Yellow
     Write-Host " EXPIRD..." -ForegroundColor red
         }
 $results.EndDate=$req.ServicePoint.Certificate.GetExpirationDateString()
 $Fullresult+=$results
+$Fullresult
 }
 Catch{
 Write-Host $URL -NoNewline -ForegroundColor red " -- ERROR --> " $_.exception.Message
@@ -42,9 +54,5 @@ $Fullresult+=$results
 
 }
 Write-Host "`nThe Full result are as the following"
-$Fullresult
-
-
-
-
+$Fullresult | ft
 
